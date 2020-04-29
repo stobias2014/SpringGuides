7c6a2314ae1dd052cacb6ba1ccde72c8e0dbf692
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-29 01:34:46 -0500

    added validation form proj

---
+++ ValidationForm/src/test/java/com/tobias/saul/ValidationForm/ValidationFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.ValidationForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
