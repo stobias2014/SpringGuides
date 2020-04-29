8a55bef5513d56276895c8829e2765cf2a7df08f
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-29 00:45:18 -0500

    added upload form template

--- MultiPartFileUpload/src/main/java/com/tobias/saul/MultiPartFileUpload/MultiPartFileUploadApplication.java
+++ MultiPartFileUpload/src/main/java/com/tobias/saul/MultiPartFileUpload/MultiPartFileUploadApplication.java
@@ -1,13 +1,25 @@
 package com.tobias.saul.MultiPartFileUpload;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.tobias.saul.MultiPartFileUpload.service.StorageService;
 
 @SpringBootApplication
 public class MultiPartFileUploadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultiPartFileUploadApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
